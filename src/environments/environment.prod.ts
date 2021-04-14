25e8f83ed44592f4b178241ed141f14046c4ffa4
Dimadp1991 <57724297+Dimadp1991@users.noreply.github.com>
2021-04-14 12:12:56 +0300

    com_14-4-21

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,3 +2,11 @@ export const environment = {
   production: true,
   apiUrl:'https://masab-api.herokuapp.com/'
 };
+
+
+//runnig api from local host
+//http://127.0.0.1:8000/
+
+
+//running api from heroku
+//https://masab-api.herokuapp.com/
